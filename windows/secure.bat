d7c00bd0b5e47ed49f0655fa920a93899a4c3e08
enzod <enzod@DESKTOP-EU1LPFJ>
2021-01-11 13:46:13 -0500

    removed admin2 for manual, fixed comments

--- windows/secure.bat
+++ windows/secure.bat
@@ -1,106 +1,102 @@
-# Run this script in an elevated CMD prompt
-# This does not include firewall rules
-
-# Backup admin account
-net user admin2
-net localgroup Administrators admin2 /add
+REM  Run this script in an elevated CMD prompt
+REM  This does not include firewall rules
 
 
-#rename + disable administrator account
-wmic useraccount where “name=’Administrator’” rename Admin
+REM rename + disable administrator account
+wmic useraccount where "name='Administrator'" rename Admin
 net user Admin /active:no
 
 
-#Enable full auditing
+REM Enable full auditing
 auditpol /set /category:* /success:enable /failure:enable
 
 
-# delete scheduled tasks
+REM  delete scheduled tasks
 schtasks /delete /tn *
 
 
-#Disable RDP
+REM Disable RDP
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 1 /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v fLogonDisabled /t REG_DWORD /d 1 /f
 
 
-#Disable Admin Shares (psexec)
+REM Disable Admin Shares (psexec)
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters\" /v AutoShareWks /t REG_DWORD /d 0 /f
 
 
-#SMBv1 Disable 
+REM SMBv1 Disable 
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Services\LanmanServer\Parameters" /v SMB1 /t REG_DWORD /d 1 /f
 
 
-#Temp Folder Permissioning (might break installers)
+REM Temp Folder Permissioning (might break installers)
 icacls C:\Windows\Temp /inheritance:r /deny "Everyone:(OI)(CI)(F)"
 
 
-#Hashing
+REM Hashing
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v NoLMHash /t REG_DWORD /d 1 /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v LMCompatibilityLevel /t REG_DWORD /d 5 /f
 
 
-#Anon Login
+REM Anon Login
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v restrictanonymous /t REG_DWORD /d 1 /f
 
 
-#Disable Keys
+REM Disable Keys
 reg add "HKCU\Control Panel\Accessibility\StickyKeys" /v Flags /t REG_SZ /d 506 /f
 reg add "HKCU\Control Panel\Accessibility\ToggleKeys" /v Flags /t REG_SZ /d 58 /f
 reg add "HKCU\Control Panel\Accessibility\Keyboard Response" /v Flags /t REG_SZ /d 122 /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI" /v ShowTabletKeyboard /v REG_DWORD /d 0 /f
 
 
-#pagefile wipe on shutdown
+REM pagefile wipe on shutdown
 reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v ClearPageFileAtShutdown /t REG_DWORD /d 1 /f
 
 
-#disable floppy disk remoting
+REM disable floppy disk remoting
 reg ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AllocateFloppies /t REG_DWORD /d 1 /f
 
 
-#Prevent print driver installs 
+REM Prevent print driver installs 
 reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Print\Providers\LanMan Print Services\Servers" /v AddPrinterDrivers /t REG_DWORD /d 1 /f
 
 
-#local account blank passwords
+REM local account blank passwords
 reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v LimitBlankPasswordUse /t REG_DWORD /d 1 /f
 
 
-# enable full uac
+REM  enable full uac
 reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v PromptOnSecureDesktop /t REG_DWORD /d 1 /f
 
 
-# Enable installer detections
+REM  Enable installer detections
 reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableInstallerDetection /t REG_DWORD /d 1 /f
 
 
-# anon enumeration prevention
+REM  anon enumeration prevention
 reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v restrictanonymous /t REG_DWORD /d 1 /f 
 reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v restrictanonymoussam /t REG_DWORD /d 1 /f 
 
 
-#domain cred storing 
+REM domain cred storing 
 reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v disabledomaincreds /t REG_DWORD /d 1 /f 
-#no perms to anons
+REM no perms to anons
 reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v everyoneincludesanonymous /t REG_DWORD /d 0 /f 
 
 
-#smb strengtheners
+REM smb strengtheners
 reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanWorkstation\Parameters /v EnablePlainTextPassword /t REG_DWORD /d 0 /f
 reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v NullSessionPipes /t REG_MULTI_SZ /d "" /f
 reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v NullSessionShares /t REG_MULTI_SZ /d "" /f
 
 
-#remote registry path denial
+REM remote registry path denial
 reg ADD HKLM\SYSTEM\CurrentControlSet\Control\SecurePipeServers\winreg\AllowedExactPaths /v Machine /t REG_MULTI_SZ /d "" /f
 reg ADD HKLM\SYSTEM\CurrentControlSet\Control\SecurePipeServers\winreg\AllowedPaths /v Machine /t REG_MULTI_SZ /d "" /f
 
 
-# Enable LSASS Memory Protection
+REM  Enable LSASS Memory Protection
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v RunAsPPL /t REG_DWORD /d 1 /f
 
 
-# Turn off Test Mode (in case they set the flag)
+REM  Turn off Test Mode (in case they set the flag)
 bcdedit /set testsigning off

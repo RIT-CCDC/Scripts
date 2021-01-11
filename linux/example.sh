d605585e6ce92570d3990fbb4a332470d7df959a
PapaChain <braydenjw00@gmail.com>
2021-01-11 15:55:47 -0500

    Updated Firewall Example

--- linux/example.sh
+++ linux/example.sh
@@ -1,28 +1,36 @@
 #!/bin/sh
 
-# May Have to mess around with firewalld
-# sudo systemctl stop firewalld
-# sudo sysytemctl disable firewalld
+## May Have to mess around with firewalld ## 
+    # sudo systemctl stop firewalld
+    # sudo sysytemctl disable firewalld
 
-# Flush Tables (MAKE SURE TYPED CORRECTLY)
+# If this Script is not Working check .bashrc or aliases
+
+# Flush Tables 
+echo "> Flushing Tables"
 iptables -F
 iptables -X
 
 # Accept by default in case of flush
+echo "> Applying Default Accept"
 iptables -P INPUT ACCEPT
 iptables -P OUTPUT ACCEPT
 
-# Allow ICMP
+
+# Allow ICMP 
+echo "> Allow ICMP"
 iptables -A INPUT -p ICMP -j ACCEPT
 iptables -A OUTPUT -p ICMP -j ACCEPT
 
-# Allow Loopback
+# Allow Loopback Traffic
+echo "> Allow Loopback Traffic"
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A OUTPUT -o lo -j ACCEPT
 
-# Accept SSH
+# Allow Incoming SSH
+echo "> Allow Inbound SSH"
 iptables -A INPUT -p tcp --dport ssh -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp --sport ssh -m state --state ESTABLISHED -j ACCEPT
 
 ########################
 # OTHER OPTIONAL RULES #
@@ -35,28 +43,41 @@ iptables -A OUTPUT -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
 # iptables -A OUTPUT -s 10.x.x.0/24 -j DENY
 
 # # Allow HTTP Outgoing
+# echo "> Allow Outbound HTTP"
 # iptables -A OUTPUT -p tcp --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
 # iptables -A INPUT -p tcp --sport 80 -m state --state ESTABLISHED -j ACCEPT
 
-# # Allow DNS OutGoing
+# # Allow DNS Outgoing (UDP Only)
+# echo "> Allow Outbound DNS (UDP)"
 # iptables -A OUTPUT -p udp --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT
 # iptables -A INPUT  -p udp --sport 53 -m state --state ESTABLISHED -j ACCEPT
 
-# Accept Various Port
+# # Allow SSH Outgoing
+# echo "> Allow Outbound SSH"
+# iptables -A OUTPUT -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
+# iptables -A INPUT -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
+
+# # Accept Various Port Incoming
+# echo "> Allow Outbound HTTP"
 # iptables -A INPUT -p tcp --dport 3000 -m state --state NEW,ESTABLISHED -j ACCEPT
 # iptables -A OUTPUT -p tcp --sport 3000 -m state --state ESTABLISHED -j ACCEPT
 
-# RDP
-# iptables -A INPUT -p tcp --dport 3389 -m state --state NEW,ESTABLISHED -j ACCEPT
-# iptables -A OUTPUT -p tcp --sport 3389 -m state --state ESTABLISHED -j ACCEPT
+# # Allow Various Port Outgoing
+# iptables -A OUTPUT -p udp --dport 3000 -m state --state NEW,ESTABLISHED -j ACCEPT
+# iptables -A INPUT  -p udp --sport 3000 -m state --state ESTABLISHED -j ACCEPT
+
+
 
 # Drop All Traffic If Not Matching
+echo "> Drop non-matching traffic : Connection may drop"
 iptables -A INPUT -j DROP
 iptables -A OUTPUT -j DROP
 
-# Backup Rules (Change Name for Web/No_Web)
+# Backup Rules (iptables-restore < backup)
+echo "> Back up rules"
 iptables-save >/etc/ip_rules
 
 # Anti-Lockout Rule
 sleep 3
 iptables -F
+echo "> Anti-Lockout executed : Rules have been flushed"
